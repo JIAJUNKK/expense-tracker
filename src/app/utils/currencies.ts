ef7440d5642eb9bb15d5bc4e455e2b1330481567
JiaJun <kongjiajun040103@gmail.com>
2025-02-22 22:23:53 +0000

    added AUD currency

--- src/app/utils/currencies.ts
+++ src/app/utils/currencies.ts
@@ -1,10 +1,11 @@
 import { Currency } from "./app.model";
 
 export const CURRENCIES: Currency[] = [
-    { name: 'Malaysian Ringgit', abbreviation: 'MYR', symbol: 'RM', flag: '🇲🇾' },
     { name: 'US Dollar', abbreviation: 'USD', symbol: '$', flag: '🇺🇸' },
+    { name: 'Malaysian Ringgit', abbreviation: 'MYR', symbol: 'RM', flag: '🇲🇾' },
     { name: 'British Pound', abbreviation: 'GBP', symbol: '£', flag: '🇬🇧' },
-    { name: 'Japanese Yen', abbreviation: 'JPY', symbol: '¥', flag: '🇯🇵' },
+    { name: 'Australian Dollar', abbreviation: 'AUD', symbol: 'AU$', flag: '🇦🇺' },
     { name: 'Euro', abbreviation: 'EUR', symbol: '€', flag: '🇪🇺' },
+    { name: 'Japanese Yen', abbreviation: 'JPY', symbol: '¥', flag: '🇯🇵' },
     { name: 'Chinese Yuan', abbreviation: 'CNY', symbol: '¥', flag: '🇨🇳' },
 ];
